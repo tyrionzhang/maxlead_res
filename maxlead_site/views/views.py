4ace0aaebb9db5139cdb68526345280651a6e5a7
wei.zhang <wei.zhang@weizhimen.com>
2017-11-30 11:24:28 +0800

    review 定时任务

--- maxlead_site/views/views.py
+++ maxlead_site/views/views.py
@@ -1,16 +1,20 @@
-import os,difflib
+import os,sched
 from datetime import *
-from threading import Timer
+import time
 from django.contrib.admin.views.decorators import staff_member_required
 from django.shortcuts import render
 from maxlead_site.models import UserAsins,AsinReviews,Reviews
 from maxlead import settings
 
-def RunReview(request):
-    work_path = settings.SPIDER_URL
-    os.chdir(work_path)  # 修改当前工作目录
-    os.system('scrapyd-deploy')
-    os.system('curl http://localhost:6800/schedule.json -d project=maxlead_scrapy -d spider=review_spider')
+# 第一个参数确定任务的时间，返回从某个特定的时间到现在经历的秒数
+# 第二个参数以某种人为的方式衡量时间
+schedule = sched.scheduler(time.time, time.sleep)
+
+# def RunReview(request):
+    # work_path = settings.SPIDER_URL
+    # os.chdir(work_path)  # 修改当前工作目录
+    # os.system('scrapyd-deploy')
+    # os.system('curl http://localhost:6800/schedule.json -d project=maxlead_scrapy -d spider=review_spider')
 
     # aid_list = UserAsins.objects.filter(is_use=True).values('aid')
     # for aid in list(aid_list):
@@ -27,11 +31,28 @@ def RunReview(request):
     #                 diff = d.compare(content_line, con_line)
     #                 print(list(diff))
     #     pass
+    # review_time = settings.REVIEW_TIME
+    # run()
+    # global t
+    # t = Timer(300.0, RunReview(request))
+    # t.start()
+
+def perform_command():
+    # 安排inc秒后再次运行自己，即周期运行
     review_time = settings.REVIEW_TIME
-    t = Timer(review_time, RunReview(request))
-    t.start()
+    schedule.enter(review_time, 0, perform_command)
+    os.system('curl http://localhost:6800/schedule.json -d project=maxlead_scrapy -d spider=review_spider')
 
+
+def RunReview(request):
+    work_path = settings.SPIDER_URL
+    os.chdir(work_path)
+    os.system('scrapyd-deploy')
+    # enter用来安排某事件的发生时间，从现在起第n秒开始启动
+    review_time = settings.REVIEW_TIME
+    schedule.enter(review_time, 0, perform_command)
+    # 持续运行，直到计划时间队列变成空为止
+    schedule.run()
     return render(request, 'spider/home.html')
-
 
 RunReview = staff_member_required(RunReview)
