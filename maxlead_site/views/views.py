67619bf63cbe7a4dbfaa7a3621570ead3e3ccfae
wei.zhang <wei.zhang@weizhimen.com>
2017-12-05 15:55:01 +0800

    review 启动先爬一次

--- maxlead_site/views/views.py
+++ maxlead_site/views/views.py
@@ -58,6 +58,7 @@ def RunReview(request):
     os.chdir(work_path)
     os.system('scrapyd-deploy')
     # enter用来安排某事件的发生时间，从现在起第n秒开始启动
+    perform_command()
     review_time = settings.REVIEW_TIME
     review_key = review_time+300
     schedule.enter(review_time, 0, perform_command)
