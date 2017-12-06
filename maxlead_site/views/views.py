6d6378111b502eb1c750786f7465e5db9cde00b4
Administrator <123456>
2017-12-06 14:24:39 +0800

    review 启动时爬一次

--- maxlead_site/views/views.py
+++ maxlead_site/views/views.py
@@ -11,10 +11,9 @@ from maxlead import settings
 # 第二个参数以某种人为的方式衡量时间
 schedule = sched.scheduler(time.time, time.sleep)
 
-def update_kewords(first=False):
-    if not first:
-        review_time = settings.REVIEW_TIME+300
-        schedule.enter(review_time, 0, update_kewords)
+def update_kewords():
+    review_time = settings.REVIEW_TIME+300
+    schedule.enter(review_time, 0, update_kewords)
 
     aid_list = UserAsins.objects.filter(is_use=True).values('aid')
     positive_keywords = ''
@@ -53,6 +52,34 @@ def perform_command():
     os.chdir(work_path)
     os.system('curl http://localhost:6800/schedule.json -d project=maxlead_scrapy -d spider=review_spider')
 
+def update_kewords1():
+    aid_list = UserAsins.objects.filter(is_use=True).values('aid')
+    positive_keywords = ''
+    negative_keywords = ''
+    for aid in list(aid_list):
+        review_contents = Reviews.objects.filter(asin=aid['aid']).filter(created=date.today()).filter(
+            score__gte=3).values('content')
+        nega_review_contents = Reviews.objects.filter(asin=aid['aid']).filter(created=date.today()).exclude(
+            score__gte=3).values('content')
+        asin_reviews = AsinReviews.objects.filter(aid=aid['aid']).filter(created=date.today()).all()
+        if asin_reviews:
+            if review_contents:
+                review_contents = list(review_contents)
+            posi_line_list = commons.get_diff_str(review_contents)
+            nega_line_list = commons.get_diff_str(nega_review_contents)
+
+            if posi_line_list:
+                for posi in set(posi_line_list):
+                    if posi_line_list.count(posi) >= 2:
+                        positive_keywords += posi + '||'
+            if nega_line_list:
+                for nega in set(nega_line_list):
+                    if nega_line_list.count(nega) >= 2:
+                        negative_keywords += nega + '||'
+            if positive_keywords:
+                asin_reviews.update(positive_keywords=positive_keywords)
+            if negative_keywords:
+                asin_reviews.update(negative_keywords=negative_keywords)
 
 def RunReview(request):
     work_path = settings.SPIDER_URL
@@ -60,7 +87,7 @@ def RunReview(request):
     os.system('scrapyd-deploy')
     # enter用来安排某事件的发生时间，从现在起第n秒开始启动
     os.system('curl http://localhost:6800/schedule.json -d project=maxlead_scrapy -d spider=review_spider')
-    schedule.enter(300, 0, update_kewords(first=True))
+    schedule.enter(300, 0, update_kewords1)
 
     review_time = settings.REVIEW_TIME
     review_key = review_time+300
