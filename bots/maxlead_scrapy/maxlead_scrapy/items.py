a76997154bd9f9d025ef0d06a28f405f900dcb6e
wei.zhang <wei.zhang@weizhimen.com>
2018-05-18 14:29:49 +0800

    测试代码

--- bots/maxlead_scrapy/maxlead_scrapy/items.py
+++ bots/maxlead_scrapy/maxlead_scrapy/items.py
@@ -43,10 +43,4 @@ class ListingWacherItem(DjangoItem):
 class CategoryRankItem(DjangoItem):
     django_model = CategoryRank
 
-class FileDownloadItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    file_urls=scrapy.Field()
-    files=scrapy.Field()
-
 
