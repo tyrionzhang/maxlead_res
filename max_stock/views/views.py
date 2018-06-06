d6830a53c7e21138addc2abdfb42c7ed7d9bd0ca
wei.zhang <wei.zhang@weizhimen.com>
2018-06-06 12:01:28 +0800

    stocks,check stocks

--- max_stock/views/views.py
+++ max_stock/views/views.py
@@ -1,6 +1,11 @@
 import os,sched
 from datetime import *
+import queue
+import threading
 import time
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
+from maxlead_site.views.app import App
 from maxlead import settings
 from max_stock.models import SkuUsers,StockLogs,WarehouseStocks
 from django.http import HttpResponse
@@ -22,29 +27,59 @@ def _set_user_sku():
             f.close()
     return True
 
-def perform_command():
-    # 安排inc秒后再次运行自己，即周期运行
-    spiders_time = settings.STOCHS_TIME
-    schedule.enter(spiders_time, 0, perform_command)
-    _set_user_sku()
+class perform_command_que(threading.Thread):
+    def __init__(self, t_name, queue):
+        threading.Thread.__init__(self,name=t_name)
+        self.data = queue
+        self.t_name = t_name
 
-    work_path = settings.STOCHS_SPIDER_URL
-    os.chdir(work_path)
-    os.popen('scrapyd-deploy')
-    cmd_str2 = 'curl http://localhost:6800/schedule.json -d project=stockbot -d spider=twu_spider'
-    cmd_str1 = 'curl http://localhost:6800/schedule.json -d project=stockbot -d spider=hanover_spider'
-    cmd_str3 = 'curl http://localhost:6800/schedule.json -d project=stockbot -d spider=exl_spider'
-    os.popen(cmd_str2)
-    os.popen(cmd_str1)
-    os.popen(cmd_str3)
-    os.chdir(settings.ROOT_PATH)
+    def run(self):
+        work_path = settings.STOCHS_SPIDER_URL
+        os.chdir(work_path)
+        os.popen('scrapyd-deploy')
+
+        cmd_str2 = 'curl http://localhost:6800/schedule.json -d project=stockbot -d spider=twu_spider'
+        cmd_str1 = 'curl http://localhost:6800/schedule.json -d project=stockbot -d spider=hanover_spider'
+        cmd_str3 = 'curl http://localhost:6800/schedule.json -d project=stockbot -d spider=exl_spider'
+        os.popen(cmd_str2)
+        os.popen(cmd_str1)
+        os.popen(cmd_str3)
+        print('%s:%s finished!' % (time.time(), self.getName()))
+        os.chdir(settings.ROOT_PATH)
+
+def run_command_queue():
+    spiders_time = "%.1f" % settings.STOCHS_TIME
+    t = threading.Timer(float(spiders_time), run_command_queue)
+    _set_user_sku()
+    q = queue.Queue()
+
+    tname = 'stocks_done'
+    reviews = perform_command_que(tname, q)
+    reviews.start()
+    reviews.join()
+    t.start()
 
 def stock_spiders(request):
-    schedule.enter(1, 0, perform_command)
-    # 持续运行，直到计划时间队列变成空为止
-    print('Spiders is runing!Time:%s' % datetime.now())
-    schedule.run()
-    return HttpResponse(request, 'Spiders is runing!Time:%s' % datetime.now())
+    user = App.get_user_info(request)
+    if not user:
+        return HttpResponseRedirect("/admin/max_stock/login/")
+    type = request.GET.get('type','')
+    if type == 'now':
+        _set_user_sku()
+        q = queue.Queue()
+
+        tname = 'stocks_done'
+        reviews = perform_command_que(tname, q)
+        reviews.start()
+        reviews.join()
+        msg_str = 'Spiders is runing!'
+    else:
+        t = threading.Timer(60.0, run_command_queue)
+        # 持续运行，直到计划时间队列变成空为止
+        t.start()
+        time_str = datetime.now() +  timedelta(seconds = 60)
+        msg_str = 'Spiders will be runing!The time:%s' % time_str
+    return render(request, "Stocks/spider/home.html", {'msg_str':msg_str})
 
 def save_logs(data):
     logs = StockLogs()
