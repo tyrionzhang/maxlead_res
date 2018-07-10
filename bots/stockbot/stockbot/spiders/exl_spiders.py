c3070072d07ebdd7a7e0bba2123db890f360a6ab
tyrionzhang <swlxyztd@163.com>
2018-07-10 17:32:26 +0800

    chrome

--- bots/stockbot/stockbot/spiders/exl_spiders.py
+++ bots/stockbot/stockbot/spiders/exl_spiders.py
@@ -61,10 +61,16 @@ class ExlSpider(scrapy.Spider):
             print(length)
             for i in range(0, length):
                 if not i == 0:
-                    driver.get('https://secure-wms.com/PresentationTier/StockStatusReport.aspx')
-                    a_res = driver.find_elements_by_id('CustomerFacilityGrid_div-rows')
-                    del list_rows
-                    list_rows = a_res[0].find_elements_by_class_name('aw-text-normal')
+                    driver.get(response.url)
+                    del a_reports, a_stock, rows_res, list_rows
+                    a_reports = driver.find_elements_by_id('Menu_Reports_head')
+                    if a_reports:
+                        a_reports[0].click()
+                    a_stock = driver.find_elements_by_css_selector('#Menu_Reports a')
+                    if a_stock:
+                        a_stock[0].click()
+                    rows_res = driver.find_elements_by_id('CustomerFacilityGrid_div-rows')
+                    list_rows = rows_res[0].find_elements_by_class_name('aw-text-normal')
                 warehouse_name = list_rows[i].find_elements_by_id('CustomerFacilityGrid_div-cell-1-%s' % i)
                 if warehouse_name:
                     warehouse_name = warehouse_name[0].text
