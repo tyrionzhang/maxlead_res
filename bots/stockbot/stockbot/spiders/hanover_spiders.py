95d65fea2941c29f14e539ab8cc76600ffc3dd54
tyrionzhang <swlxyztd@163.com>
2018-07-10 11:26:59 +0800

    chrome

--- bots/stockbot/stockbot/spiders/hanover_spiders.py
+++ bots/stockbot/stockbot/spiders/hanover_spiders.py
@@ -32,6 +32,9 @@ class HanoverSpider(scrapy.Spider):
     def parse(self, response):
         file_path = os.path.join(max_settings.BASE_DIR, max_settings.THRESHOLD_TXT, 'threshold_txt.txt')
         msg_str2 = ''
+        from pyvirtualdisplay import Display
+        display = Display(visible=0, size=(800, 800))
+        display.start()
         chrome_options = Options()
         chrome_options.add_argument('headless')
         chrome_options.add_argument('--disable-gpu')
@@ -75,6 +78,7 @@ class HanoverSpider(scrapy.Spider):
                         if user:
                             msg_str2 += '%s=>SKU:%s,Warehouse:%s,QTY:%s,Early warning value:%s \n|' % ( user[0].user.email,
                                                     item['sku'], item['warehouse'], item['qty'], threshold[0].threshold)
+        display.stop()
         driver.quit()
 
         if not os.path.isfile(file_path):
