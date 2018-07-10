30c9f18306d0340cb5427f0343d6307a88b5da1a
tyrionzhang <swlxyztd@163.com>
2018-07-10 17:48:03 +0800

    chrome

--- bots/stockbot/stockbot/spiders/exl_spiders.py
+++ bots/stockbot/stockbot/spiders/exl_spiders.py
@@ -61,16 +61,12 @@ class ExlSpider(scrapy.Spider):
             for i in range(0, length):
                 if not i == 0:
                     driver.get(response.url)
-                    del a_reports, a_stock, rows_res, list_rows
-                    a_reports = driver.find_elements_by_id('Menu_Reports_head')
-                    a_reports[0].click()
-                    if a_reports:
-                        a_reports[0].click()
-                    a_stock = driver.find_elements_by_css_selector('#Menu_Reports a')
-                    if a_stock:
-                        a_stock[0].click()
-                    rows_res = driver.find_elements_by_id('CustomerFacilityGrid_div-rows')
-                    list_rows = rows_res[0].find_elements_by_class_name('aw-text-normal')
+                    a_report = driver.find_elements_by_id('Menu_Reports_head')
+                    a_report[0].click()
+                    a_stocks = driver.find_elements_by_css_selector('#Menu_Reports a')
+                    a_stocks[0].click()
+                    rows_re = driver.find_elements_by_id('CustomerFacilityGrid_div-rows')
+                    list_rows = rows_re[0].find_elements_by_class_name('aw-text-normal')
                 warehouse_name = list_rows[i].find_elements_by_id('CustomerFacilityGrid_div-cell-1-%s' % i)
                 if warehouse_name:
                     warehouse_name = warehouse_name[0].text
