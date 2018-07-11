7863c0a266f894c56067797bb3d0b8eedbe96f59
tyrionzhang <swlxyztd@163.com>
2018-07-11 14:06:14 +0800

    chrome

--- max_stock/views/stocks.py
+++ max_stock/views/stocks.py
@@ -51,7 +51,7 @@ def index(request):
             have_new = 1
         if old:
             if not new:
-                have_new = 1
+                re.update({'is_new_type': 1})
             qty_old = old[0].qty
             re.update({'qty_old':qty_old,'created':old[0].created.strftime("%Y-%m-%d %H:%M:%S")})
 
