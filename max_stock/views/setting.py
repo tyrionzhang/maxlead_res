af922d540668d8926a001ad895bc4e950688e5f0
tyrionzhang <swlxyztd@163.com>
2018-07-23 15:53:44 +0800

    菜单管理

--- max_stock/views/setting.py
+++ max_stock/views/setting.py
@@ -174,7 +174,7 @@ def get_role_user(request):
         for val in user_list:
             if val.userprofile.stocks_role == role_code and role_code:
                 right_str += '<option value="%s">%s</option>' % (val.id, val.username)
-            elif not val.userprofile.stocks_role:
+            elif val.userprofile.stocks_role == '0' and not val.is_superuser:
                 left_str += '<option value="%s">%s</option>' % (val.id, val.username)
         data = {'right_str': right_str, 'left_str': left_str}
         return HttpResponse(json.dumps({'code': 1, 'data': data}), content_type='application/json')
