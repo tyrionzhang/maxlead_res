9e6980a8f96739cc6c51a1f11a8e9e15fd73c571
tyrionzhang <swlxyztd@163.com>
2018-07-25 17:38:06 +0800

    chrome listing,review拉取

--- bots/maxlead_scrapy/maxlead_scrapy/spiders/review_spider.py
+++ bots/maxlead_scrapy/maxlead_scrapy/spiders/review_spider.py
@@ -39,13 +39,14 @@ class ReviewSpider(scrapy.Spider):
         else:
             check = 1
             asin_id = res_asin[4]
-        # from pyvirtualdisplay import Display
-        # display = Display(visible=0, size=(800, 800))
-        # display.start()
+        from pyvirtualdisplay import Display
+        display = Display(visible=0, size=(800, 800))
+        display.start()
         chrome_options = Options()
         chrome_options.add_argument('-headless')
         chrome_options.add_argument('--disable-gpu')
-        driver = webdriver.Chrome(chrome_options=chrome_options)
+        driver = webdriver.Chrome(chrome_options=chrome_options, executable_path=settings.CHROME_PATH,
+                                  service_log_path=settings.LOG_PATH)
         driver.get(response.url)
         driver.implicitly_wait(100)
         next_page = driver.find_elements_by_css_selector('li.a-last a')
@@ -101,6 +102,8 @@ class ReviewSpider(scrapy.Spider):
             re = AsinReviews.objects.filter(aid=asin_id,created__icontains=datetime.datetime.now().strftime('%Y-%m-%d'))
             if re:
                 re.update(is_done=1)
+
+        display.stop()
         driver.quit()
 
     def parse_details(self, response):
