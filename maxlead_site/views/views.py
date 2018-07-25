fa78dceae7da166bba5a188c6c63ff42eb661b6a
tyrionzhang <swlxyztd@163.com>
2018-07-25 13:56:46 +0800

    chrome listing拉取

--- maxlead_site/views/views.py
+++ maxlead_site/views/views.py
@@ -314,7 +314,7 @@ def test_spider(request):
     os.chdir(work_path)
     os.popen('scrapyd-deploy')
 
-    cmd_str2_test = 'curl http://localhost:6800/schedule.json -d project=maxlead_scrapy -d spider=test_spider -d asin=%s' % 88
+    cmd_str2_test = 'curl http://localhost:6800/schedule.json -d project=maxlead_scrapy -d spider=listing1_spider -d asin=%s' % 88
     os.popen(cmd_str2_test)
     return render(request, "Stocks/spider/home.html", {'msg_str': 'Done!'})
 
