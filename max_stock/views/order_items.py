4dab0aa0dcc52a10cdbdeaaed1ce70ce2dd52724
tyrionzhang <swlxyztd@163.com>
2018-08-16 10:56:35 +0800

    send email time

--- max_stock/views/order_items.py
+++ max_stock/views/order_items.py
@@ -54,7 +54,7 @@ def send_email_as_tmp(title, msg, from_email, email):
     msg['Subject'] = Header(title, 'utf-8').encode()
     msg['date'] = time.strftime("%a,%d %b %Y %H:%M:%S %z")
     # 发送邮件
-    server = smtplib.SMTP(smtp_server, 25)
+    server = smtplib.SMTP(smtp_server, 465)
     server.set_debuglevel(1)
     server.login(from_addr, password)
     server.sendmail(from_addr, to_addr, msg.as_string())
