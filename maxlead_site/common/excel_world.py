b2a9023cf9414884f634ef44f20d3fce49cdc2b7
tyrionzhang <swlxyztd@163.com>
2018-08-31 18:15:43 +0800

    spiders2 run time

--- maxlead_site/common/excel_world.py
+++ maxlead_site/common/excel_world.py
@@ -237,22 +237,25 @@ def read_excel_file1(model,res,model_name,user=None):
                     continue
 
             for n,val in enumerate(fields,0):
-                if not n == 0:
+                if n == 1 and user:
                     a = '%s,'
                     a1 = "\'%s\',"
-                    val_res = table.cell_value(i + 1, n-1,)
+                    val_res = user
+                    val.name = 'user_id'
+                    str1 += a % val.name
+                    str2 += a1 % val_res
+                if not n == 0 and not n == 1:
+                    a = '%s,'
+                    a1 = "\'%s\',"
+                    val_res = table.cell_value(i + 1, n-2,)
                     if n+1 == len(fields):
                         a = '%s'
                         a1 = "\'%s\'"
                     if val.name == 'user_id' or val.name == 'user':
-                        if user:
-                            val_res = user
+                        user_obj = User.objects.filter(username=val_res)
+                        if user_obj:
+                            val_res = user_obj[0].id
                             val.name = 'user_id'
-                        else:
-                            user_obj = User.objects.filter(username=val_res)
-                            if user_obj:
-                                val_res = user_obj[0].id
-                                val.name = 'user_id'
                     if val.name == 'send_time':
                         val_res = xlrd.xldate.xldate_as_datetime(val_res, 0).strftime("%H:%M")
                     if val.get_internal_type() == 'DateTimeField':
