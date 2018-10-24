c3f0458401599dad61c7fdc7ca30aabb54c5b83d
tyrionzhang <swlxyztd@163.com>
2018-10-24 17:27:31 +0800

    warehouse 数据导出时间筛选

--- bots/stockbot/stockbot/spiders/exl_spiders.py
+++ bots/stockbot/stockbot/spiders/exl_spiders.py
@@ -14,18 +14,19 @@ class ExlSpider(scrapy.Spider):
     msg_str1 = 'complete\n'
     start_urls = ['https://secure-wms.com/PresentationTier/LoginForm.aspx?3pl={073abe7b-9d71-414d-9933-c71befa9e569}']
     sku_list = []
+    stock_names = ['M&L','Match Land']
 
-    def __init__(self, username=None, *args, **kwargs):
-        super(ExlSpider, self).__init__(*args, **kwargs)
-        file_name = 'userSkus_txt.txt'
-        if username:
-            file_name = 'userSkus_txt_%s.txt' % username
-        file_path = os.path.join(max_settings.BASE_DIR, max_settings.THRESHOLD_TXT, file_name)
-        with open(file_path, "r") as f:
-            sku_list = f.read()
-            f.close()
-        if sku_list:
-            self.sku_list = eval(sku_list)
+    # def __init__(self, username=None, *args, **kwargs):
+    #     super(ExlSpider, self).__init__(*args, **kwargs)
+    #     file_name = 'userSkus_txt.txt'
+    #     if username:
+    #         file_name = 'userSkus_txt_%s.txt' % username
+    #     file_path = os.path.join(max_settings.BASE_DIR, max_settings.THRESHOLD_TXT, file_name)
+    #     with open(file_path, "r") as f:
+    #         sku_list = f.read()
+    #         f.close()
+    #     if sku_list:
+    #         self.sku_list = eval(sku_list)
 
     def parse(self, response):
         file_path = os.path.join(max_settings.BASE_DIR, max_settings.THRESHOLD_TXT, 'threshold_txt.txt')
@@ -44,9 +45,9 @@ class ExlSpider(scrapy.Spider):
         # sel_stock = driver.find_elements_by_id('StockStatusViewer__ctl1__ctl5__ctl0')
 
         if elem_name:
-            elem_name[0].send_keys('Maxlead_CS')
+            elem_name[0].send_keys('Intybot')
         if elem_pass:
-            elem_pass[0].send_keys('2015dallas')
+            elem_pass[0].send_keys('7G1#AJjX')
         btn_login[0].click()
         a_reports = driver.find_elements_by_id('Menu_Reports_head')
         if a_reports:
@@ -90,43 +91,46 @@ class ExlSpider(scrapy.Spider):
                     list_rows = driver.find_elements_by_css_selector('#CustomerFacilityGrid_div-rows>span')
                     list_rows.pop(0)
                     list_rows.pop(-1)
-                warehouse_name = list_rows[i].find_elements_by_id('CustomerFacilityGrid_div-cell-1-%s' % i)
-                if warehouse_name:
-                    warehouse_name = warehouse_name[0].text
-                list_rows[i].click()
-                btn_runreport = driver.find_elements_by_id('btnRunRpt')
-                if btn_runreport:
-                    btn_runreport[0].click()
-                iframe1 = driver.find_elements_by_id('ReportFrameStockStatusViewer')
-                if iframe1:
-                    driver.switch_to.frame(iframe1[0])
-                iframe2 = driver.find_elements_by_id('report')
-                driver.switch_to.frame(iframe2[0])
-                driver.implicitly_wait(100)
-                res = driver.find_elements_by_css_selector('.a383 tr')
-                res.pop(1)
-                res.pop(0)
-                res.pop()
-                for val in res:
-                    item = WarehouseStocksItem()
-                    tds = val.find_elements_by_tag_name('td')
-                    if tds:
-                        item['sku'] = tds[0].text
-                        item['warehouse'] = warehouse_name
-                        if warehouse_name == 'Exchange Logistics':
-                            item['warehouse'] = 'EXL'
-                        if tds[6].text and not tds[6].text == ' ':
-                            item['qty'] = tds[6].text
-                            item['qty'] = item['qty'].replace(',', '')
-                        else:
-                            item['qty'] = 0
-                        yield item
-                        threshold = Thresholds.objects.filter(sku=item['sku'],warehouse=item['warehouse'])
-                        user = SkuUsers.objects.filter(sku=item['sku'])
-                        if threshold and threshold[0].threshold >= int(item['qty']):
-                            if user:
-                                msg_str2 += '%s=>SKU:%s,Warehouse:%s,QTY:%s,Early warning value:%s \n|' % (user[0].user.email,
-                                                            item['sku'], item['warehouse'], item['qty'], threshold[0].threshold)
+                warehouse_type = list_rows[i].find_elements_by_id('CustomerFacilityGrid_div-cell-0-%s' % i)
+                warehouse_type_name = warehouse_type[0].text
+                if warehouse_type_name in self.stock_names:
+                    warehouse_name = list_rows[i].find_elements_by_id('CustomerFacilityGrid_div-cell-1-%s' % i)
+                    if warehouse_name:
+                        warehouse_name = warehouse_name[0].text
+                    list_rows[i].click()
+                    btn_runreport = driver.find_elements_by_id('btnRunRpt')
+                    if btn_runreport:
+                        btn_runreport[0].click()
+                    iframe1 = driver.find_elements_by_id('ReportFrameStockStatusViewer')
+                    if iframe1:
+                        driver.switch_to.frame(iframe1[0])
+                    iframe2 = driver.find_elements_by_id('report')
+                    driver.switch_to.frame(iframe2[0])
+                    driver.implicitly_wait(100)
+                    res = driver.find_elements_by_css_selector('.a383 tr')
+                    res.pop(1)
+                    res.pop(0)
+                    res.pop()
+                    for val in res:
+                        item = WarehouseStocksItem()
+                        tds = val.find_elements_by_tag_name('td')
+                        if tds:
+                            item['sku'] = tds[0].text
+                            item['warehouse'] = warehouse_name
+                            if warehouse_name == 'Exchange Logistics':
+                                item['warehouse'] = 'EXL'
+                            if tds[6].text and not tds[6].text == ' ':
+                                item['qty'] = tds[6].text
+                                item['qty'] = item['qty'].replace(',', '')
+                            else:
+                                item['qty'] = 0
+                            yield item
+                            threshold = Thresholds.objects.filter(sku=item['sku'],warehouse=item['warehouse'])
+                            user = SkuUsers.objects.filter(sku=item['sku'])
+                            if threshold and threshold[0].threshold >= int(item['qty']):
+                                if user:
+                                    msg_str2 += '%s=>SKU:%s,Warehouse:%s,QTY:%s,Early warning value:%s \n|' % (user[0].user.email,
+                                                                item['sku'], item['warehouse'], item['qty'], threshold[0].threshold)
         display.stop()
         driver.quit()
 
