7b0a61dcd83ae18e9d3ae88fc3dd3e656add6584
tyrionzhang <swlxyztd@163.com>
2018-11-21 15:31:25 +0800

    Default Changelist

--- maxlead_site/views/listing/listing.py
+++ maxlead_site/views/listing/listing.py
@@ -23,19 +23,21 @@ class Listing:
         work_path = settings.SPIDER_URL
         os.chdir(work_path)
         os.system('scrapyd-deploy')
+        aid_li = ''
         for val in asins:
             checks = UserAsins.objects.filter(aid=val)
+            aid_li += '%s|' % val
             if len(checks) == 1:
                 cmd_str = 'curl http://localhost:6800/schedule.json -d project=maxlead_scrapy -d spider=review_spider -d asin=%s' % val
-                cmd_str1 = 'curl http://localhost:6800/schedule.json -d project=maxlead_scrapy -d spider=listing_spider -d asin=%s' % val
                 cmd_str2 = 'curl http://localhost:6800/schedule.json -d project=maxlead_scrapy -d spider=catrank_spider -d asin=%s' % val
                 cmd_str3 = 'curl http://localhost:6800/schedule.json -d project=maxlead_scrapy -d spider=qa_spider -d asin=%s' % val
                 cmd_str4 = 'curl http://localhost:6800/schedule.json -d project=maxlead_scrapy -d spider=watcher_spider -d asin=%s' % val
                 os.system(cmd_str)
-                os.system(cmd_str1)
                 os.system(cmd_str2)
                 os.system(cmd_str3)
                 os.system(cmd_str4)
+        cmd_str1 = 'curl http://localhost:6800/schedule.json -d project=maxlead_scrapy -d spider=listing_spider -d asin=%s' % aid_li
+        os.system(cmd_str1)
         os.chdir(settings.ROOT_PATH)
         return HttpResponse(json.dumps({'code': 1, }), content_type='application/json')
 
