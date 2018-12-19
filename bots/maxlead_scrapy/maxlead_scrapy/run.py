f05dcbaa1639750ae62d78d6b5bb3fdec4b94e65
tyrionzhang <swlxyztd@163.com>
2018-12-19 11:15:44 +0800

    Default Changelist

--- bots/maxlead_scrapy/maxlead_scrapy/run.py
+++ bots/maxlead_scrapy/maxlead_scrapy/run.py
@@ -3,7 +3,8 @@
 from scrapy import cmdline
 
 
-name = 'listing_spider'
-asin = 'B00BOYG4WE'
-cmd = 'scrapy crawl {0} -a asin={1}'.format(name,asin)
+name = 'twu_spider'
+# asin = '88'
+# cmd = 'scrapy crawl {0} -a asin={1}'.format(name,asin)
+cmd = 'scrapy crawl {0}'.format(name)
 cmdline.execute(cmd.split())
