28f35c5400fbd728ea0e22aad9e533f1716e5e68
tyrionzhang <swlxyztd@163.com>
2018-12-26 10:30:30 +0800

    Default Changelist

--- maxlead/wsgi.py
+++ maxlead/wsgi.py
@@ -17,15 +17,14 @@ from django.core.wsgi import get_wsgi_application
 from maxlead_site.views.views import download_listings,get_send_time,spiders2
 from max_stock.views.views import run_command_queue,task_save_stocks
 
-t = threading.Timer(1.0, spiders2)
-t.start()
+os.popen('scrapyd')
 
 time_re5 = int(get_send_time('08:00'))
-# time_re6 = time_re5 + 1800
 t4 = threading.Timer(float('%.1f' % time_re5), run_command_queue)
 t4.start()
-# t5 = threading.Timer(float('%.1f' % time_re6), task_save_stocks)
-# t5.start()
+
+t = threading.Timer(79200.0, spiders2)
+t.start()
 
 # os.environ.setdefault("DJANGO_SETTINGS_MODULE", "maxlead.settings")
 os.environ['DJANGO_SETTINGS_MODULE'] = 'maxlead.settings'
