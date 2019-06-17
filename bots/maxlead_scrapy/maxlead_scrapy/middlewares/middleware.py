6d5d6823b7efbc986a328ae42e9be8f0b7cc200f
tyrionzhang <swlxyztd@163.com>
2019-06-17 10:20:35 +0800

    Default Changelist

--- bots/maxlead_scrapy/maxlead_scrapy/middlewares/middleware.py
+++ bots/maxlead_scrapy/maxlead_scrapy/middlewares/middleware.py
@@ -3,53 +3,29 @@ import random
 from scrapy import log
 import logging
 from scrapy.downloadermiddlewares.useragent import UserAgentMiddleware
-from maxlead_site.common.ip_util import IPUtil
+from fake_useragent import UserAgent
 
 
-class UserAgent(UserAgentMiddleware):
-    user_agent_list = [
-        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 "
-        "(KHTML, like Gecko) Chrome/22.0.1207.1 Safari/537.1",
-        "Mozilla/5.0 (X11; CrOS i686 2268.111.0) AppleWebKit/536.11 "
-        "(KHTML, like Gecko) Chrome/20.0.1132.57 Safari/536.11",
-        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.6 "
-        "(KHTML, like Gecko) Chrome/20.0.1092.0 Safari/536.6",
-        "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.6 "
-        "(KHTML, like Gecko) Chrome/20.0.1090.0 Safari/536.6",
-        "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.1 "
-        "(KHTML, like Gecko) Chrome/19.77.34.5 Safari/537.1",
-        "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/536.5 "
-        "(KHTML, like Gecko) Chrome/19.0.1084.9 Safari/536.5",
-        "Mozilla/5.0 (Windows NT 6.0) AppleWebKit/536.5 "
-        "(KHTML, like Gecko) Chrome/19.0.1084.36 Safari/536.5",
-        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.3 "
-        "(KHTML, like Gecko) Chrome/19.0.1063.0 Safari/536.3",
-        "Mozilla/5.0 (Windows NT 5.1) AppleWebKit/536.3 "
-        "(KHTML, like Gecko) Chrome/19.0.1063.0 Safari/536.3",
-        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_0) AppleWebKit/536.3 "
-        "(KHTML, like Gecko) Chrome/19.0.1063.0 Safari/536.3",
-        "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.3 "
-        "(KHTML, like Gecko) Chrome/19.0.1062.0 Safari/536.3",
-        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.3 "
-        "(KHTML, like Gecko) Chrome/19.0.1062.0 Safari/536.3",
-        "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.3 "
-        "(KHTML, like Gecko) Chrome/19.0.1061.1 Safari/536.3",
-        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.3 "
-        "(KHTML, like Gecko) Chrome/19.0.1061.1 Safari/536.3",
-        "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/536.3 "
-        "(KHTML, like Gecko) Chrome/19.0.1061.1 Safari/536.3",
-        "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.3 "
-        "(KHTML, like Gecko) Chrome/19.0.1061.0 Safari/536.3",
-        "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/535.24 "
-        "(KHTML, like Gecko) Chrome/19.0.1055.1 Safari/535.24",
-        "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/535.24 "
-        "(KHTML, like Gecko) Chrome/19.0.1055.1 Safari/535.24"
-    ]
+class RandomUserAgentMiddleware(UserAgentMiddleware):
+    def __init__(self, crawler):
+        super(RandomUserAgentMiddleware, self).__init__()
+        self.ua = UserAgent()
+        # 从settings.py中读取RANDOM_UA_TYPE配置 如果没有则默认值为random  达到可配置的目的
+        # 默认是random随机选择，但是可以在配置指定ie或者firefox、chrome等浏览器的不同版本
+        self.ua_type = crawler.settings.get("RANDOM_UA_TYPE", "random")
 
-    def __init__(self, user_agent=''):
-        self.user_agent = user_agent
+    @classmethod
+    def from_crawler(cls, crawler):
+        return cls(crawler)
 
     def process_request(self, request, spider):
+        def get_ua():
+            """
+            函数中的函数 闭包
+            读取上面的ua_type设置 让process_request直接调用本get_ua
+            """
+            return getattr(self.ua, self.ua_type)
+
         headers = {
             "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
             "Accept-Language": "zh-CN,zh;q=0.8,en-US;q=0.5,en;q=0.3",
@@ -58,7 +34,7 @@ class UserAgent(UserAgentMiddleware):
             "Connection": "keep - alive",
             "Access-Control-Allow-Origin": "*"
         }
-        ua = random.choice(self.user_agent_list)
+        ua = get_ua()
         if ua:
             res_asin = request.url.split('aid=')
             qid = request.url.split('qid=')
@@ -77,18 +53,3 @@ class UserAgent(UserAgentMiddleware):
             request.headers.setdefault('Accept-Language', headers['Accept-Language'])
             request.headers.setdefault('Accept-Encoding', headers['Accept-Encoding'])
             request.headers.setdefault('Access-Control-Allow-Origin', headers['Access-Control-Allow-Origin'])
-            # ip_util = IPUtil()
-            # proxy_ip = ip_util.get_random_ip()
-            # print('using ip proxy:', proxy_ip)
-            # request.meta["proxy"] = proxy_ip
-
-
-# class IPPOOLS(HttpProxyMiddleware):
-#
-#     def __init__(self, ip=''):
-#         self.ip = ip
-#
-#     def process_request(self, request, spider):
-#         thisip = random.choice(IPPOOL)
-#         print("this is ip:" + thisip["ipaddr"])
-#         request.meta["proxy"] = "http://" + thisip["ipaddr"]
