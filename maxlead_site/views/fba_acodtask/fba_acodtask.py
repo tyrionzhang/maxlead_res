75e7575d56dc7aa20e4b57ca26e80e22911ebc5e
tyrionzhang <swlxyztd@163.com>
2019-10-25 10:30:31 +0800

    Default Changelist

--- maxlead_site/views/fba_acodtask/fba_acodtask.py
+++ maxlead_site/views/fba_acodtask/fba_acodtask.py
@@ -101,7 +101,7 @@ def fba_import(request):
         for chunk in myfile.chunks():
             f.write(chunk)
         f.close()
-        file = open(file_path, 'r', encoding='utf-8')
+        file = open(file_path, 'r')
         csv_files = csv.reader(file)
         msg = 'Successfully!\n'
         file_name1 = 'Fba-Account-customer%s.xlsx' % (datetime.datetime.now().strftime('%Y-%m-%d %H%M%S'))
