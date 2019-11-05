ddb6ad8af86d2d6d5f75417ee8f1adff2cc7c1ea
tyrionzhang <swlxyztd@163.com>
2019-11-05 09:42:08 +0800

    Default Changelist

--- bots/stockbot/stockbot/spiders/zto_spiders.py
+++ bots/stockbot/stockbot/spiders/zto_spiders.py
@@ -43,7 +43,7 @@ class ZtoSpider(scrapy.Spider):
         if elem_name:
             elem_name[0].send_keys('ZTLO')
         if elem_pass:
-            elem_pass[0].send_keys('usZTO5012')
+            elem_pass[0].send_keys('uszto50124th')
         btn_login[0].click()
         driver.implicitly_wait(100)
         time.sleep(3)
