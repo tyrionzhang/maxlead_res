08f956e1a3e04a27f7a3740693afb29978da1867
wei.zhang <wei.zhang@weizhimen.com>
2020-01-21 15:12:10 +0800

    stocks only

--- maxlead_site/common/excel_world.py
+++ maxlead_site/common/excel_world.py
@@ -15,12 +15,6 @@ import sys
 importlib.reload(sys)
 import os.path
 
-from pdfminer.pdfparser import  PDFParser,PDFDocument
-from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter, process_pdf
-from pdfminer.converter import PDFPageAggregator, TextConverter
-from pdfminer.layout import LTTextBoxHorizontal,LAParams
-from pdfminer.pdfinterp import PDFTextExtractionNotAllowed
-
 
 def handleEncoding(original_file):
     with open(original_file, 'rb+') as fp:
@@ -468,65 +462,6 @@ def read_excel_for_tracking_orders(res,user=None):
                 msg += '第%s行添加有误。<br>' % (i + 1)
                 continue
     return {'code': 1, 'msg': msg}
-
-def readPDF(pdfFile):
-    rsrcmgr = PDFResourceManager()
-    retstr = StringIO()
-    laparams = LAParams()
-    device = TextConverter(rsrcmgr, retstr, laparams=laparams)
-
-    process_pdf(rsrcmgr, device, pdfFile)
-    device.close()
-
-    content = retstr.getvalue()
-    retstr.close()
-    # 获取所有行
-    lines = str(content).split("\n")
-    return content
-
-def read_excel_data_for_pdf(model,res):
-    fields = model._meta.fields
-    '''解析PDF文本'''
-    fp = open(res, 'rb')
-    outputString = readPDF(fp)
-    print(outputString)
-    # 用文件对象创建一个PDF文档分析器
-    parser = PDFParser(fp)
-    # 创建一个PDF文档
-    doc = PDFDocument()
-    # 连接分析器，与文档对象
-    parser.set_document(doc)
-    doc.set_parser(parser)
-
-    # 提供初始化密码，如果没有密码，就创建一个空的字符串
-    doc.initialize()
-
-    # 检测文档是否提供txt转换，不提供就忽略
-    if not doc.is_extractable:
-        raise PDFTextExtractionNotAllowed
-    else:
-        # 创建PDF，资源管理器，来共享资源
-        rsrcmgr = PDFResourceManager()
-        # 创建一个PDF设备对象
-        laparams = LAParams()
-        device = PDFPageAggregator(rsrcmgr, laparams=laparams)
-        # 创建一个PDF解释其对象
-        interpreter = PDFPageInterpreter(rsrcmgr, device)
-
-        # 循环遍历列表，每次处理一个page内容
-        # doc.get_pages() 获取page列表
-        for page in doc.get_pages():
-            interpreter.process_page(page)
-            # 接受该页面的LTPage对象
-            layout = device.get_result()
-            # 这里layout是一个LTPage对象 里面存放着 这个page解析出的各种对象
-            # 一般包括LTTextBox, LTFigure, LTImage, LTTextBoxHorizontal 等等
-            # 想要获取文本就获得对象的text属性，
-            for x in enumerate(layout, 0):
-                if (isinstance(x, LTTextBoxHorizontal)):
-                    results = x.get_text()
-                    print(results)
-
 
 def read_ads_excel(file, user, range_type=None, week=None, month=None, account=None, type=None):
     from django.db import connection, transaction
