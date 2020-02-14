38bc8791142c303ecdfdb056360407586198e2cb
wei.zhang <wei.zhang@weizhimen.com>
2020-02-14 10:58:21 +0800

    stocks only

--- bots/stockbot/stockbot/spiders/exl_spiders.py
+++ bots/stockbot/stockbot/spiders/exl_spiders.py
@@ -92,10 +92,12 @@ class ExlSpider(scrapy.Spider):
                 try:
                     if not i == 0:
                         driver.refresh()
-                        driver.switch_to.alert.accept()
                         driver.implicitly_wait(100)
                         time.sleep(3)
                         driver.get('https://secure-wms.com/WebUI/V1/V1Link/StockStatusReport.aspx')
+                        driver.implicitly_wait(100)
+                        time.sleep(3)
+                        driver.refresh()
                         driver.implicitly_wait(100)
                         time.sleep(3)
                         list_rows = driver.find_elements_by_css_selector('#CustomerFacilityGrid_div-rows>span')
